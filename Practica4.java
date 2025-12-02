b0f39ad11daddaf0f32999b09faa32f81d29a70d
Pablo Gundin Fernández <pablogf30@educastur.es>
2025-12-02 20:33:25 +0100

    Esta es la practica 4 de java actualizada, con comentarios dentro del codigo y contando cuantas veces se genero un numero distinto al que estaba antes

--- Practica4.java
+++ Practica4.java
@@ -5,26 +5,33 @@ public class Practica4 {
 
         Scanner sc = new Scanner(System.in);
 
+        // El programa muestra por pantalla el proceso de generar numeros al azar
         System.out.println("Generando numeros al azar...");
 
         int contador = 0;
 
         int numero = (int) (Math.random() * 100) + 1;
 
-        int nvecesnumero12 = 0;
+
+        // Mientras que se hayan contado menos de 100 numeros y que el numero generado no sea 99 (Porque se detendria el programa), el programa generara numeros al azar del 1 al 100
+
+        int nvecesnumero30 = 0;
         while ((contador < 100) && (numero != 99)) {
             numero = (int) (Math.random() * 100) + 1;
             System.out.println("Numero generado: " + numero);
             contador++;
 
-            if (numero == 12){
-                nvecesnumero12++;
+            //Si se genera el numero 30, se suma la vez que salio
+
+            if (numero == 30){
+                nvecesnumero30++;
             }
 
 
         }
 
-        System.out.println("Se genero el numero 12 un total de "+nvecesnumero12+" veces");
+        //Se imprime por pantalla si salio el numero 30 alguna vez
+        System.out.println("Se genero el numero 12 un total de "+nvecesnumero30+" veces");
             }
 
 
